d5be7497b4d279f5a6a9f8d6b88dc041d5f8f889
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-11-02 18:47:59 -0400

    file expansion

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -l
 set -uxo pipefail
 
+# Avoid file expansion when passing parameters like with '*'
+set -o noglob
+
 OUTPUT=${OUTPUT:="git-cliff/CHANGELOG.md"}
 
 # Create the output directory
 mkdir -p "$(dirname $OUTPUT)"
 
 # Separate arguments before passing them to git-cliff command
-args=$(echo $@ | xargs)
+args=$(echo "$@" | xargs)
 
 # Execute git-cliff
 GIT_CLIFF_OUTPUT="$OUTPUT" git-cliff $args
